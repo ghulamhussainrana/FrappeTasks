7512973d45d29f2687af81260481a16047f89c8c
Ghulam Hussain <92977746+ghulamhussainrana@users.noreply.github.com>
2023-07-07 14:16:34 +0500

    Add files via upload

--- tasks.js
+++ tasks.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2023, hafiz and contributors
 // For license information, please see license.txt
 
+// Tasks 1-4 (3/7/2023 - 5/7/2023)
 frappe.ui.form.on('Tasks', {
 	refresh: function(frm) {
 
@@ -32,6 +33,8 @@ frappe.ui.form.on('Tasks', {
 					frm.set_value('dob', values.popup_date);
 			}, 'Popup to change values in fields');
 		});
+
+		// query to filter supplier based on supplier group
 		frm.set_query('query_supplier', function() {	
 			return {
 				filters: {
@@ -41,6 +44,7 @@ frappe.ui.form.on('Tasks', {
 		}
 		);
 
+		// Query method to filter supplier based on supplier group
 		frm.set_query('query_method_supplier', function() {	
 			return {
 				query: 'hafiz_app.programming_module.doctype.tasks.tasks.custom_query_method',
@@ -50,6 +54,83 @@ frappe.ui.form.on('Tasks', {
 			}
 		}
 		);
+	},
 
-	}
+
+
+// Tasks 1-4 (5/7/2023 - 7/7/2023)
+	// Hide and unhide fields based on checkbox
+	// and making fields mandatory based on checkbox
+	onload: function(frm) {
+		frm.toggle_display(['phone','query_method_supplier'], false);
+	},
+	enable: function(frm) {
+        // Check the condition and hide/unhide fields accordingly
+        if (frm.doc.enable) {
+            frm.toggle_display(['phone','query_method_supplier'], true);  // Show fields
+			frm.toggle_reqd('name1', true);
+            // frm.toggle_display('dob', false); // Hide fields
+        }
+		else {
+			frm.toggle_display(['phone','query_method_supplier'], false);  // Hide fields 
+			frm.toggle_reqd('name1', false);
+			// frm.toggle_display('phone', false); // Hide fields
+			// frm.toggle_display('dob', true);  // Show fields
+		}
+		
+		// if (frm.doc.enable) {
+        //     frm.toggle_display(['phone', 'dob'], true); 
+        //     frm.toggle_display(['query_supplier', 'query_method_supplier'], false);
+        // } else {
+		// 	frm.toggle_display(['phone', 'dob'], false); 
+        //     frm.toggle_display(['query_supplier', 'query_method_supplier'], true);
+        // }
+    },
+
+	query_supplier: function(frm) {
+        var link_value = frm.doc.query_supplier;
+
+        if (link_value) {
+            // Make an AJAX request to fetch data from the Python function
+            frappe.call({
+                method: 'hafiz_app.programming_module.doctype.tasks.tasks.load_data_from_linked_doctype',
+                args: {
+                    doctype: 'Supplier',
+                    docname: link_value
+                },
+                callback: function(response) {
+                    var data = response.message;
+                    // Populate the fields in the current form with the returned data
+                    if (data) {
+                        frm.set_value('supplier_type_link', data.type);
+                    }
+                }
+            });
+        }
+		else{
+			frm.set_value('supplier_type_link', '');
+		}
+    },
+
+	validate: function(frm) {
+		console.log("Hello This is 'Validate Event'")
+	},
+	// before_save: function(frm) {
+	// 	console.log("Hello This is 'Before Save Event'")
+	// },
+	// after_save: function(frm) {
+	// 	console.log("Hello This is 'After Save Event'")
+	// },
+	// before_submit: function(frm) {
+	// 	console.log("Hello This is 'Before Submit Event'")
+	// }
+	// on_submit: function(frm) {
+	// 	console.log("Hello This is 'On Submit Event'")
+	// }
+	// before_cancel: function(frm) {
+	// 	console.log("Hello This is 'Before Cancel Event'")
+	// },
+	// after_cancel: function(frm){
+	// 	console.log("Hello This is 'After Cancel Event'")
+	// }
 });
--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from frappe.model.document import Document
 
 class Tasks(Document):
 	pass
-
+# Tasks 1-4 (3/7/2023 - 5/7/2023)
 @frappe.whitelist()
 def custom_query_method(doctype, txt, searchfield, start, page_len, filters):
 	query = """
@@ -22,3 +22,21 @@ def custom_query_method(doctype, txt, searchfield, start, page_len, filters):
 
 # def get_supplier(self):
 # 	frappe.msgprint(f"Supplier name")
+
+
+
+
+# Tasks 1-4 (5/7/2023 - 7/7/2023)
+
+@frappe.whitelist()
+def load_data_from_linked_doctype(doctype, docname):
+    linked_doc = frappe.get_doc(doctype, docname)
+    
+    # Access the fields from the linked document and return them as a dictionary
+    if linked_doc:
+        data = {
+            'type': linked_doc.supplier_type
+        }
+        return data
+    else:
+        return {}
